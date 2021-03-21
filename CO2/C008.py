25d7c21eb9be7144b55e80d70790d2f6920a1f2f
anilachacko <76002746+anilachacko@users.noreply.github.com>
2021-03-21 20:27:27 +0530

    CO2

---
+++ CO2/C008.py
@@ -0,0 +1,14 @@
+a=[]
+n= int(input("Enter the number of elements in list:"))
+for x in range(0,n):
+    element=input("Enter element" + str(x+1) + ":")
+    a.append(element)
+max1=len(a[0])
+temp=a[0]
+for i in a:
+    if(len(i)>max1):
+       max1=len(i)
+       temp=i
+print("The longest word is:")
+print(temp)
+print("with length is" , len(temp))
---
+++ CO2/c0011.py
@@ -0,0 +1,9 @@
+square = lambda s : s*s
+rectangle = lambda l,b : l*b
+triangle = lambda b,h : (b*h)/2
+print("Area of square" ,square(5))
+print("Area of triangle", triangle(7,11))
+print("Area of rectangle", rectangle(5,6))
+
+
+
